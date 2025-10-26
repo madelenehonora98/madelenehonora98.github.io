3136e434386214e450d4e2c7897867c286806b42
madelenehonora98 <madelenehonora98@gmail.com>
2025-10-27 00:36:46 +0700

    update code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx,scss}'],
+  content: ['./src/**/*.{vue,js,ts,jsx,tsx,scss}'],
   theme: {
     extend: {
       colors: {
